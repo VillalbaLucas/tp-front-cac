b9959bd70f047290d4d72eb5133f88431c9e3287
VillalbaLucas <lukasvillalba2016@gmail.com>
2023-05-16 23:01:41 -0300

    create: toggle interfaz home and  ticket form

--- main.js
+++ main.js
@@ -1,12 +1,37 @@
-function comprarTicket(){
-    console.log("comprando ticket")
+const ticketInterfaz = document.querySelector('#ticketInterfaz')
+const homeInterfaz = document.querySelector('#homeInterfaz')
 
-    const header = document.querySelector("header")
+ticketInterfaz.addEventListener('click', mostrarInterfaz);
+homeInterfaz.addEventListener('click', mostrarHome);
 
-    const template = templateTicket.content.cloneNode(true)
-    const div = template.querySelector("div")
-    // const span = document.createElement("span");
-    div.innerHTML = "Seleccion de tickets"
+const header = document.querySelector("header")
+const headerClases = header.classList
 
-    list.appendChild(template)
+function mostrarInterfaz() {
+    removeHeader();
+    console.log("comprando ticket");
+
+    // const template = templateTicket.content.cloneNode(true)
+    // const div = template.querySelector("div")
+    // // const span = document.createElement("span");
+    // div.innerHTML = "Seleccion de tickets"
+
+    // list.appendChild(template)
+}
+
+
+function mostrarHome(headerClases) {
+    if (!header.classList == 'header-hidden') {
+        return
+    }
+    alert("Volviendo a la pagina de inicio")
+    header.classList.remove('header-hidden')
+    console.log(header.classList)
+    header.classList.add
+    console.log(header.classList)
+
+}
+
+function removeHeader() {
+    header.classList.value = 'header-hidden'
 }
