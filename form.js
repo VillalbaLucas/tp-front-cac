bb41dc3b87369f8920415ecdea573410aeb01925
VillalbaLucas <lukasvillalba2016@gmail.com>
2023-05-23 00:38:27 -0300

    refact: inputs and create: validate inputs with regular exp

--- form.js
+++ form.js
@@ -1,20 +1,24 @@
-const input = form.querySelectorAll('#floatingInput')
-const cantidad = document.querySelector('#inputCantidad')
-const inputTotal = document.querySelector('#disabledTextInput')
+const form = document.forms.formulario
+const inputs = form.querySelectorAll('input')
 
 const porcentaje = new Map()
 porcentaje.set('estudiante', 80)
 porcentaje.set('trainee', 50)
 porcentaje.set('junior', 15)
 
-function comprar() {
+function comprar(){
 
-    if(cantidad.value <= 0 || isNaN(cantidad.value)) return alert('Ingrese una cantidad valida')
+    let cantidad = inputs[3].value,
+        inputTotal = inputs[4],
+        name = inputs[0].value,
+        lastname = inputs[1].value
 
-    let select = document.querySelector('#select').value
-    let cantPrecio = cantidad.value * 200
+    if(cantidad <= 0 || !validarCampo(name) || !validarCampo(lastname) ) return alert('Ingrese datos validos')
 
-    let total =  cantPrecio - ( cantPrecio * porcentaje.get(select))/100
+    let select = form.querySelector('#select').value,
+        cantPrecio = cantidad * 200,
+        total =  cantPrecio - ( cantPrecio * porcentaje.get(select))/100
+
     inputTotal.value = parseInt(total)
    
     const card = createInfoCard(total)
@@ -22,7 +26,7 @@ function comprar() {
 }
 
 function borrar(){
-    input.forEach(i => i.value = '')
+    inputs.forEach(i => i.value = '')
 }
 
 const mostrarInfoCard = (card) => {
@@ -43,9 +47,9 @@ const createInfoCard = (monto) => {
     card.innerHTML = `
         <h5 class="pb-4 border-bottom " >Informacion de la compra</h5>
         <p>Monto total a pagar: $${monto}</p>
-        <p>Nombre: ${input[0].value}</p>
-        <p>Apellido: ${input[1].value}</p>
-        <p>Email: ${input[2].value}</p>
+        <p>Nombre: ${inputs[0].value}</p>
+        <p>Apellido: ${inputs[1].value}</p>
+        <p>Email: ${inputs[2].value}</p>
         <p>Fecha de compra: ${fecha}</p>
         <p>Hora de compra: ${hora}</p>
         <button class="btn btn-success" onClick="removeCard(cardInfo)" >Aceptar</button>
@@ -61,4 +65,11 @@ const removeCard = (card) => {
     section.style.pointerEvents = 'auto'
     templateMain.removeChild(card)
     borrar()
+}
+
+// Validaciones de formularios
+
+validarCampo = (cadena) => {
+    const nameAccepted = /([a-z]+|[A-Z]+[a-z])/
+    return nameAccepted.test(cadena)
 }
